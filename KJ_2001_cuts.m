975638bac51a00f87d302c9fdc25a8ffb6ea0579
Kieselfried <Kieselfried@users.noreply.github.com>
2016-05-22 01:40:03 +0200

    Create KJ_2001_cuts.m

---
+++ KJ_2001_cuts.m
@@ -0,0 +1,263 @@
+%% TODO
+% + automatische Stationsstarts (über m_maps?)
+% + Interpolation der Bodenwerte und geschätzte Linie für Meeresboden
+% + bessere Anpassung (der Farbskala) an temp, salt, oxy
+% * Anpassung für andere Variablen-Pakete (POS294, POS303, KJ2001)
+
+%% Start
+clc
+clear all
+close all
+load KJ_2001_ctd
+
+%%% FARBACHSEN EVENTUELL BEI DEN SUBPLOTS KORRIGIEREN
+
+%% globale Einstellungen & feste Variablen
+Xs = ctd_station;
+Ys = ctd_prs(:,1:1179);
+Os = ctd_oxy(:,1:1179);
+Ts = ctd_temp(:,1:1179);
+Ss = ctd_salt(:,1:1179);
+
+% Stationen bei
+Start = [1  15 38 50 71 82  98 109 124 134 155 161 164 167];
+End   = [35 19 47 54 81 97 108 123 134 154 160 163 166 170];
+
+%%
+% +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+%%% GESAMTPLOT
+% +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+%% gesamt_Temperatur
+
+
+% Anzahl der Konturlinien regulieren
+% hierzu Vektor mit Wertebereich definieren
+lo_lim_t=round(nanmin(nanmin(Ts')));
+up_lim_t=round(nanmax(nanmax(Ts')));
+res_t=1;
+s_scale_t=lo_lim_t:res_t:up_lim_t;
+% Konturlinien hervorheben
+s_line_t=[0 10];
+
+figure(1)
+subplot(2,2,1)
+contourf(Xs,Ys(130,:),Ts')
+% Y Skala umdrehen
+set(gca,'YDir','reverse')
+
+% Farbskala 
+%h_t=colorbar('fontsize',12,'fontweight','b');
+% Farbpalette an Zahl der Konturlinien anpassen
+% Farbpalette ist "jet"
+cm=colormap(jet(length(s_scale_t)-1));
+% Farbachse lässt sich so verschieben
+caxis([min(s_scale_t) max(s_scale_t)]);
+hold on
+% Konturlinien hervorheben
+[C h]=contour(Xs,Ys(130,:),Ts',s_line_t,'k','linewidth',3);
+% Konturlinien beschriften
+hl=clabel(C,h);
+set(hl,'FontSize',10,'FontWeight','demi','color','k');
+% Box als Hintergrund für Label malen
+set(hl,'BackgroundColor',[1 1 1],'Edgecolor',[.7 .7 .7]);
+xlabel('Messstation')
+ylabel('Druck')
+title(' Darstellung der Temperatur')
+
+%% gesamt_Sauerstoffgehalt
+
+% Anzahl der Konturlinien regulieren
+% hierzu Vektor mit Wertebereich definieren
+lo_lim_o=round(nanmin(nanmin(Os')));
+up_lim_o=round(nanmax(nanmax(Os')));
+res_o=1;
+s_scale_o=lo_lim_o:res_o:up_lim_o;
+% Konturlinien hervorheben
+s_line_o=0:1:13;
+
+
+figure(1)
+subplot(2,2,2)
+contourf(Xs,Ys(130,:),Os')
+% Y Skala umdrehen
+set(gca,'YDir','reverse')
+
+% Farbskala 
+h=colorbar('fontsize',12,'fontweight','b');
+% Farbpalette an Zahl der Konturlinien anpassen
+% Farbpalette ist "jet"
+cm=colormap(jet(length(s_scale_o)-1));
+% Farbachse lässt sich so verschieben
+caxis([min(s_scale_o) max(s_scale_o)]);
+hold on
+% Konturlinien hervorheben
+[C h]=contour(Xs,Ys(130,:),Os',s_line_o,'k','linewidth',2);
+% Konturlinien beschriften
+hl=clabel(C,h);
+set(hl,'FontSize',10,'FontWeight','demi','color','k');
+% Box als Hintergrund für Label malen
+set(hl,'BackgroundColor',[1 1 1],'Edgecolor',[.7 .7 .7]);
+xlabel('Messstation')
+ylabel('Druck')
+title(' Darstellung des Sauerstoffs')
+
+%% gesamt_Salzgehalt
+
+
+% Anzahl der Konturlinien regulieren
+% hierzu Vektor mit Wertebereich definieren
+lo_lim_s=round(nanmin(nanmin(Ss')));
+up_lim_s=round(nanmax(nanmax(Ss')));
+res_s=0.25;
+s_scale_s=lo_lim_s:res_s:up_lim_s;
+% Konturlinien hervorheben
+s_line_s=31:1:35;
+
+
+figure(1)
+subplot(2,2,3)
+contourf(Xs,Ys(130,:),Ss')
+% Y Skala umdrehen
+set(gca,'YDir','reverse')
+
+% Farbskala 
+h=colorbar('fontsize',12,'fontweight','b');
+% Farbpalette an Zahl der Konturlinien anpassen
+% Farbpalette ist "jet"
+cm=colormap(jet(length(s_scale_s)-1));
+% Farbachse lässt sich so verschieben
+caxis([min(s_scale_s) max(s_scale_s)]);
+hold on
+% Konturlinien hervorheben
+[C h]=contour(Xs,Ys(130,:),Ss',s_line_s,'k','linewidth',2);
+% Konturlinien beschriften
+hl=clabel(C,h);
+set(hl,'FontSize',10,'FontWeight','demi','color','k');
+% Box als Hintergrund für Label malen
+set(hl,'BackgroundColor',[1 1 1],'Edgecolor',[.7 .7 .7]);
+xlabel('Messstation')
+ylabel('Druck')
+title(' Darstellung des Salzgehaltes')
+
+%% Als PDF Abspeichern
+
+filename = strcat ('KJ_2001_gesamt.pdf');
+%print(1,'-dpdf',filename)
+
+
+%% ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+%%% EINZELNE STATIONEN
+% +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+for Station=1:length(Start)
+%% Temperatur
+% Tiefsten Messwert finden
+[M,I]       = max(ctd_prs,[],2);
+deepest = max(I(Start(Station):End(Station)));
+
+X = Xs(Start(Station):End(Station));
+Y = Ys(130,1:deepest);
+T = Ts(Start(Station):End(Station),1:deepest)';
+
+figure(Station+1)
+subplot(2,2,1)
+contourf(X,Y,T)
+
+% Y Skala umdrehen
+set(gca,'YDir','reverse')
+% Farbskala 
+h=colorbar('fontsize',12,'fontweight','b');
+% Farbpalette an Zahl der Konturlinien anpassen
+% Farbpalette ist "jet"
+cm=colormap(jet(length(s_scale_t)-1));
+% Farbachse lässt sich so verschieben
+caxis([min(s_scale_t) max(s_scale_t)]);
+hold on
+[C h]=contour(X,Y,T,s_line_t,'k','linewidth',3);
+% Konturlinien beschriften
+hl=clabel(C,h);
+set(hl,'FontSize',10,'FontWeight','demi','color','k');
+% Box als Hintergrund für Label malen
+set(hl,'BackgroundColor',[1 1 1],'Edgecolor',[.7 .7 .7]);
+xlabel('Messstation')
+ylabel('Druck')
+title(' Darstellung der Temperatur')
+
+%% Sauerstoffgehalt
+
+% Tiefsten Messwert finden
+[M,I]       = max(ctd_prs,[],2);
+deepest = max(I(Start(Station):End(Station)));
+
+X = Xs(Start(Station):End(Station));
+Y = Ys(130,1:deepest);
+O = Os(Start(Station):End(Station),1:deepest)';
+
+figure(Station+1)
+subplot(2,2,2)
+contourf(X,Y,O)
+
+% Y Skala umdrehen
+set(gca,'YDir','reverse')
+% Farbskala 
+h=colorbar('fontsize',12,'fontweight','b');
+% Farbpalette an Zahl der Konturlinien anpassen
+% Farbpalette ist "jet"
+cm=colormap(jet(length(s_scale_o)-1));
+% Farbachse lässt sich so verschieben
+caxis([min(s_scale_o) max(s_scale_o)]);
+hold on
+% Konturlinien hervorheben
+s_line_o=0:1:13;
+[C h]=contour(X,Y,O,s_line_o,'k','linewidth',2);
+% Konturlinien beschriften
+hl=clabel(C,h);
+set(hl,'FontSize',10,'FontWeight','demi','color','k');
+% Box als Hintergrund für Label malen
+set(hl,'BackgroundColor',[1 1 1],'Edgecolor',[.7 .7 .7]);
+xlabel('Messstation')
+ylabel('Druck')
+title(' Darstellung des Sauerstoffs')
+
+%% Salzgehalt
+
+% Tiefsten Messwert finden
+[M,I]       = max(ctd_prs,[],2);
+deepest = max(I(Start(Station):End(Station)));
+
+X = Xs(Start(Station):End(Station));
+Y = Ys(130,1:deepest);
+S = Ss(Start(Station):End(Station),1:deepest)';
+
+figure(Station+1)
+subplot(2,2,3)
+contourf(X,Y,S)
+
+% Y Skala umdrehen
+set(gca,'YDir','reverse')
+% Farbskala 
+h=colorbar('fontsize',12,'fontweight','b');
+% Farbpalette an Zahl der Konturlinien anpassen
+% Farbpalette ist "jet"
+cm=colormap(jet(length(s_scale_s)-1));
+% Farbachse lässt sich so verschieben
+caxis([min(s_scale_s) max(s_scale_s)]);
+hold on
+% Konturlinien hervorheben
+s_line_s=31:1:35;
+[C h]=contour(X,Y,S,s_line_s,'k','linewidth',2);
+% Konturlinien beschriften
+hl=clabel(C,h);
+set(hl,'FontSize',10,'FontWeight','demi','color','k');
+% Box als Hintergrund für Label malen
+set(hl,'BackgroundColor',[1 1 1],'Edgecolor',[.7 .7 .7]);
+xlabel('Messstation')
+ylabel('Druck')
+title(' Darstellung des Salzgehaltes')
+
+%% Als PDF Abspeichern
+filename = strcat ('KJ_2001_cut_',num2str(Xs(Start(Station))),'_to_',num2str(Xs(End(Station))),'.pdf');
+% print(Station+1,'-dpdf',filename)
+end
+
+
+
