72f78c5003643e112bccd44b647b08b02905ab3c
Kieselfried <Kieselfried@users.noreply.github.com>
2016-06-16 21:15:41 +0200

    Update LADCP_IFR.m

--- LADCP_IFR.m
+++ LADCP_IFR.m
@@ -4,7 +4,7 @@
 
 %%Im folgenden ein erster Versuch zum Plotten der ladcp Daten in der Dänemarkstrasse:
 
-%%%Faroe Bank Channel - die Velocity Schnitte
+%%%Island Faroe Ridge - die Velocity Schnitte
 %##########################################################################
 clear all
 close all
@@ -12,141 +12,114 @@ close all
 for k=1:2
 clearvars -except k
 plot_velo=1;
-print_velo=1;
-  
+print_velo=0;
+
 Fahrt={'POS294'
        'POS303'
        'POS471'
        };
 
-% Daten laden
-% ctd={'POS294_ctd'
-%      'POS303_ctd'
-%      'pos471_ctd'
-%      };
- 
 ladcp={'pos294_ladcp'
        'pos303_ladcp'
        'pos471_ladcp'
        };
-   
-% load(ctd{k})
+
 load(ladcp{k})
 
-stations=[[766:790 816:830]; 
-          [705:736 zeros(1,8)];
-          [760:765 zeros(1,34)];
+stations=[[766 790 816 830]; % Poseidon Cruise 294
+          [705 -99 -99 736]; % Poseidon Cruise 303
+%          [760 -99 -99 765]; % Poseidon Cruise 471
           ];
 
-%% East-West
-% Variablen auf Schnitt reduzieren
-ladcp_station=ladcp_station(find(ladcp_station==stations(k,1)):find(ladcp_station==stations(k,2)));
-ladcp_lon=ladcp_lon((find(ladcp_station==stations(k,1)):find(ladcp_station==stations(k,2))));
-ladcp_lat=ladcp_lat((find(ladcp_station==stations(k,1)):find(ladcp_station==stations(k,2))));
-ladcp_depth=ladcp_depth(:,find(ladcp_station==stations(k,1)):find(ladcp_station==stations(k,2)));
-
-% absolute Geschwindigkeit berechnen
-% velo=sqrt(ladcp_u_corr.^2.*ladcp_v_corr.^2);
-% velo=ladcp_u_corr.*ladcp_v_corr;
-velo=ladcp_u_corr;
+% Variablen auf relevante Stationen reduzieren
+cutout=[ladcp_station(find(ladcp_station<stations(k,1))) ladcp_station(find(ladcp_station==stations(k,2)+1):find(ladcp_station==stations(k,3)-1)) ladcp_station(find(ladcp_station>stations(k,4)))];
+cut_stations=ladcp_station;
+cut_stations(find(ismember(ladcp_station,cutout)))=[];
+cut_ladcp_lon=ladcp_lon;
+cut_ladcp_lon(find(ismember(ladcp_station,cutout)))=[];
+cut_ladcp_lat=ladcp_lat;
+cut_ladcp_lat(find(ismember(ladcp_station,cutout)))=[];
+ladcp_depth=ladcp_depth(:,find(ladcp_station==stations(k,1)):find(ladcp_station==stations(k,end)));
 
 % Distanz zwischen Stationen (
 % Faengt bei Null an und wird in Kilometern kumulativ berechnet)
-dist=cumsum([0 gsw_distance(ladcp_lon,ladcp_lat)]/1000);
+dist=cumsum([0 gsw_distance(cut_ladcp_lon,cut_ladcp_lat)]/1000);
 
-%% East-West
 % auf gleichmaessiges Gitter interpolieren
 % vertikal in dBar
 Depth=1:1:max(max(ladcp_depth));
 
-% vertikale Interpolation
-veloI=(zeros(length(Depth),length(dist))); % VeloI erstellen um Rechenpower zu sparen
+% Arrays erstellen um Rechenpower zu sparen
+veloI=(zeros(length(Depth),length(dist)));           
+ladcp_u_corrI=(zeros(length(Depth),length(dist))); 
+ladcp_v_corrI=(zeros(length(Depth),length(dist))); 
 
-for ii = 1:length(find(ladcp_station==stations(k,1)):find(ladcp_station==stations(k,2)));
+% vertikale Interpolation
+for ii = 1:length(cut_stations(:));
+
     % Werte finden die nicht NaN sind
     ind=find(~isnan(ladcp_depth(:,ii))==1);
+
     % auf neues Gitter interpolieren
-    veloI(:,ii)=interp1(ladcp_depth(ind,ii),velo(ind,ii),Depth);
     ladcp_u_corrI(:,ii)=interp1(ladcp_depth(ind,ii),ladcp_u_corr(ind,ii),Depth);
     ladcp_v_corrI(:,ii)=interp1(ladcp_depth(ind,ii),ladcp_v_corr(ind,ii),Depth);
-
+    
 end
 
-% Schnitte Plotten
+% maximale Geschwindigkeit berechnen (fuer Colorbar Skala)
+v_max=max([max(max(abs(ladcp_u_corrI))) max(max(abs(ladcp_v_corrI)))]);
+
+% Colormap erstellen
+mincolor    = [1 0 0]; % red
+mediancolor = [1 1 1]; % white   
+maxcolor    = [0 0 1]; % blue         
+
+ColorMapSize = 16;
+int1 = zeros(ColorMapSize,3); 
+int2 = zeros(ColorMapSize,3);
+
+for i=1:3
+    int1(:,i) = linspace(mincolor(i), mediancolor(i), ColorMapSize);
+    int2(:,i) = linspace(mediancolor(i), maxcolor(i), ColorMapSize);
+end
+
+meep = [int1(1:end-1,:); int2];
+
+
+%% Schnitte Plotten (West-East)
 if plot_velo==1
     figure(k)
+    
     subplot(1,2,1)
-    contourf(dist,Depth,veloI)
+    contourf(dist,Depth,ladcp_u_corrI)
     set(gca,'YDir','reverse') 
-    set(gca,'XDir','reverse') 
     
     % Titel- und Achsenbeschriftung
     title(strcat(Fahrt(k),', Island Faroe Ridge (West-East)'))
     xlabel('Distance [km]')
     ylabel('Depth [dBar]')
-    
-    % % Farbskala anpassen
-    s_scale=-max(max(abs(veloI))):0.001:max(max(abs(veloI)));
-    cm=colormap(jet(length(s_scale)));
-    caxis([min(s_scale) max(s_scale)]);
-    cb=colorbar('southoutside');
-end
-%% North-South
-% Variablen auf Schnitt reduzieren
-ladcp_station=ladcp_station(find(ladcp_station==stations(k,1)):find(ladcp_station==stations(k,2)));
-ladcp_lon=ladcp_lon((find(ladcp_station==stations(k,1)):find(ladcp_station==stations(k,2))));
-ladcp_lat=ladcp_lat((find(ladcp_station==stations(k,1)):find(ladcp_station==stations(k,2))));
-ladcp_depth=ladcp_depth(:,find(ladcp_station==stations(k,1)):find(ladcp_station==stations(k,2)));
-
-% absolute Geschwindigkeit berechnen
-% velo=sqrt(ladcp_u_corr.^2.*ladcp_v_corr.^2);
-% velo=ladcp_u_corr.*ladcp_v_corr;
-velo=ladcp_v_corr;
-
-% Distanz zwischen Stationen (
-% Faengt bei Null an und wird in Kilometern kumulativ berechnet)
-dist=cumsum([0 gsw_distance(ladcp_lon,ladcp_lat)]/1000);
-
-%%
-% auf gleichmaessiges Gitter interpolieren
-% vertikal in dBar
-Depth=1:1:max(max(ladcp_depth));
-
-% vertikale Interpolation
-veloI=(zeros(length(Depth),length(dist))); % VeloI erstellen um Rechenpower zu sparen
-
-for ii = 1:length(find(ladcp_station==stations(k,1)):find(ladcp_station==stations(k,2)));
-    % Werte finden die nicht NaN sind
-    ind=find(~isnan(ladcp_depth(:,ii))==1);
-    % auf neues Gitter interpolieren
-    veloI(:,ii)=interp1(ladcp_depth(ind,ii),velo(ind,ii),Depth);
-    ladcp_u_corrI(:,ii)=interp1(ladcp_depth(ind,ii),ladcp_u_corr(ind,ii),Depth);
-    ladcp_v_corrI(:,ii)=interp1(ladcp_depth(ind,ii),ladcp_v_corr(ind,ii),Depth);
-
+% SKALA
+% ANPASSEN!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    colormap(meep)
+    colorbar('southoutside');
+    caxis([-v_max v_max])
 end
 
-% Schnitte Plotten
+%% Schnitte Plotten (South-North)
 if plot_velo==1
     figure(k)
     subplot(1,2,2)
-    contourf(dist,Depth,veloI)
+    contourf(dist,Depth,ladcp_v_corrI)
     set(gca,'YDir','reverse') 
-    set(gca,'XDir','reverse') 
     
     % Titel- und Achsenbeschriftung
-    title(strcat(Fahrt(k),', Island Faroe Ridge (South-North'))
+    title(strcat(Fahrt(k),', Island Faroe Ridge (South-North)'))
     xlabel('Distance [km]')
     ylabel('Depth [dBar]')
     
-    % % Farbskala anpassen
-    s_scale=-max(max(abs(veloI))):0.001:max(max(abs(veloI)));
-    cm=colormap(jet(length(s_scale)));
-    caxis([min(s_scale) max(s_scale)]);
-    cb=colorbar('southoutside');
-end
-if print_velo==1
-    filename = strcat(Fahrt(k),'_IFR_velo.png');
-    print(k,'-dpng',char(filename))
+    colormap(meep)
+    colorbar('southoutside');
+    caxis([-v_max v_max])
 end
 
 end
